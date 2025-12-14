5f988e1785ff6668b7a6d3281bf52d52af3913e2
Averal <purwaraveral@gmail.com>
2025-12-14 20:13:25 +0530

    Implement random image selection for blocks
    Randomly select image sets for block drawing.

--- js/Block.js
+++ js/Block.js
@@ -7,20 +7,27 @@ function Block()
     this.passed = false;
     this.passSound = new sound("sounds/sfx_point.mp3");
     
+    // Randomly choose which image set to use (50% chance)
+    this.useAlternate = Math.random() < 0.5;
+    
     this.drawBlock = function()
     {
         this.time ++;
         if (!over && start)
         this.X -=1.6;
         
+        // Choose which images to use based on random selection
+        var botImage = this.useAlternate ? images["bot1"] : images["bot"];
+        var topImage = this.useAlternate ? images["top1"] : images["top"];
+        
         // Draw bottom pillar (normal)
-        ctx.drawImage(images["bot"], this.X, this.botY, 52, 320);
+        ctx.drawImage(botImage, this.X, this.botY, 52, 320);
         
         // Draw top pillar (rotated 180 degrees)
         ctx.save();
         ctx.translate(this.X + 26, this.topY + 160); // Move to center of pillar
         ctx.rotate(Math.PI); // Rotate 180 degrees
-        ctx.drawImage(images["top"], -26, -160, 52, 320); // Draw centered
+        ctx.drawImage(topImage, -26, -160, 52, 320); // Draw centered
         ctx.restore();
     }   
     
